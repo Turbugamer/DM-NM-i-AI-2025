a0e892c19930c55ca261abdde2cd23c2763bde27
Kasper Jacobsen <kjjacobsen@uia.no>
2025-08-04 14:49:43 +0200

    Lagt til bitstrings

--- tumor-segmentation/test_HP.py
+++ tumor-segmentation/test_HP.py
@@ -1,32 +1,17 @@
-from scipy import ndimage
-import numpy as np
-import os
-import cv2
+from dataloder import Dataloder
 
-def main():
-    inPath = "../tumor-segmentation/data/patients/imgs"
-    outPath = "../tumor-segmentation/data/HP"
 
-    # Sørg for at output-mappen finnes
-    os.makedirs(outPath, exist_ok=True)
 
-    for filename in os.listdir(inPath):
-        if filename.endswith(".png") or filename.endswith(".jpg"):
-            full_input_path = os.path.join(inPath, filename)
+inPath = "../tumor-segmentation/data/patients/imgs"
+outPath = "../tumor-segmentation/data/HP"
 
-            # Les inn bilde
-            img = cv2.imread(full_input_path, cv2.IMREAD_GRAYSCALE)
-            if img is None:
-                print(f"Kunne ikke lese {filename}")
-                continue
 
-            # High-pass filter: original - blur + 127
-            blurred = cv2.GaussianBlur(img, (21, 21), 3)
-            hp = cv2.addWeighted(img, 1.0, blurred, -1.0, 127)
+load_obj_imgtoHP = Dataloder(inPath, outPath)
 
-            # Lagre resultat
-            full_output_path = os.path.join(outPath, filename)
-            cv2.imwrite(full_output_path, hp)
+load_obj_imgtoHP.HPconverter()        
+               
+bitStrings = load_obj_imgtoHP.convertToBitstrings()
 
-if __name__ == '__main__':
-    main()
+#Eksempel på hvordan bilde navn og bit streng henger sammen
+print(bitStrings[0][0]) # Filnavn
+print(bitStrings[0][1][:100]) # Første 100 bits
